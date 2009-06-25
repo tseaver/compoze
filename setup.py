5bb7f698cde513a0dbfba17ebec4193647904369
Tres Seaver tseaver@palladion.com <Tres Seaver tseaver@palladion.com@8f1d8bf8-68d2-4fbe-a113-2afb08c80ed9>
2009-06-25 18:06:34 +0000

    Copyright update.
    git-svn-id: svn+ssh://svn.repoze.org/svn/compoze/trunk@5592 8f1d8bf8-68d2-4fbe-a113-2afb08c80ed9

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2007 Agendaless Consulting and Contributors.
+# Copyright (c) 2007-2009 Agendaless Consulting and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the BSD-like license at
