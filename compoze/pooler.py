c722378c707f62cf721c412873342d792d676394
Tres Seaver tseaver@palladion.com <Tres Seaver tseaver@palladion.com@8f1d8bf8-68d2-4fbe-a113-2afb08c80ed9>
2009-06-27 22:36:01 +0000

    Coverage.
    git-svn-id: svn+ssh://svn.repoze.org/svn/compoze/trunk@5649 8f1d8bf8-68d2-4fbe-a113-2afb08c80ed9

--- compoze/pooler.py
+++ compoze/pooler.py
@@ -1,32 +1,29 @@
 import optparse
 import os
 import shutil
+import StringIO
 import sys
 
 ARCHIVE_EXTS = ('tar.gz', 'tgz', 'zip', 'tar.bz2', 'tbz')
 
 
-def isarchive(f):
+def is_archive(f):
     for x in ARCHIVE_EXTS:
         if f.endswith('.'+x):
             return True
     return False
 
 
-class NoArchivesException(Exception):
-    pass
-
-
 class Pooler(object):
-    '''Move all archives into a common pool directory and symlink back the
-    results.
-    '''
+    """ Move archives into a common pool directory; symlink back into path.
+    """
 
-    def __init__(self, global_options, *argv):
+    def __init__(self, global_options, *argv, **kw):
 
         argv = list(argv)
-        parser = optparse.OptionParser(
-            usage="%prog pool [OPTIONS] pool_dir\n\n  " + __doc__.strip())
+        parser = self.parser = optparse.OptionParser(
+            usage="%prog pool [OPTIONS] pool_dir\n\n  " +
+                   self.__doc__.strip())
 
         parser.add_option(
             '-q', '--quiet',
@@ -41,66 +38,80 @@ class Pooler(object):
             default=global_options.verbose,
             help="Show progress")
 
+        parser.add_option(
+            '-p', '--path',
+            action='store',
+            dest='path',
+            default='.',
+            help="Path to process")
+
         options, args = parser.parse_args(argv)
 
-        self.parser = parser
+        if len(args) != 1:
+            msg = StringIO.StringIO()
+            msg.write('No pool_dir!\n\n')
+            msg.write(parser.format_help())
+            raise ValueError(msg.getvalue())
+
         self.options = options
-        self.args = args
+        self.pool_dir = args[0]
+        self.release_dir = os.path.abspath(options.path)
+        self._logger = kw.get('logger', _print)
 
-    def __call__(self):
-        if len(self.args) == 0:
-            self.parser.print_help()
-            return
-        try:
-            all, pending = self.moveToPool(self.args[0])
-            print "Updated %i out of %i archives" % (len(pending), len(all))
-        except NoArchivesException, err:
-            print "Couldn't find any archives in '%s'" % self.releasedir
-
-    def _blather(self, text):
+    def blather(self, text):
         if self.options.verbose:
-            print text
+            self._logger(text)
 
-    @property
-    def releasedir(self):
-        return os.path.abspath(os.curdir)
-
-    @property
-    def archives(self):
+    def listArchives(self):
         all = []
         pending = []
         archives = ([], [])
-        for f in os.listdir(self.releasedir):
-            full = os.path.join(self.releasedir, f)
-            if isarchive(full) and os.path.isfile(full):
+        for filename in os.listdir(self.release_dir):
+            full = os.path.join(self.release_dir, filename)
+            if is_archive(full) and os.path.isfile(full):
                 if not os.path.islink(full):
-                    pending.append(f)
-                all.append(f)
+                    pending.append(filename)
+                all.append(filename)
         return all, pending
 
-    def moveToPool(self, pooldir):
-        all, pending = self.archives
+    def moveToPool(self):
+        all, pending = self.listArchives()
         if len(all) == 0:
-            raise NoArchivesException()
+            raise ValueError('No non-link archives in release dir: %s'
+                                % self.release_dir)
 
-        for f in pending:
-            full = os.path.join(self.releasedir, f)
-            name = f.split('-', 2)[0]
-            poolpath = os.path.join(pooldir, name.lower())
-            if not os.path.exists(poolpath):
-                os.makedirs(poolpath)
-                self._blather('Created new pool dir %s' % poolpath)
-            archivepath = os.path.join(poolpath, f)
-            if not os.path.exists(archivepath):
-                shutil.move(full, archivepath)
-                self._blather('Moved %s to %s' % (full, archivepath))
-            if os.path.exists(full):
-                os.remove(full)
-            os.symlink(archivepath, full)
+        if not os.path.exists(self.pool_dir):
+            self.blather('Created new pool dir %s' % self.pool_dir)
+            os.makedirs(self.pool_dir)
+
+        if not os.path.isdir(self.pool_dir):
+            raise ValueError('Pool dir is not a directory: %s'
+                                % self.pool_dir)
+
+        for archive in pending:
+            source = os.path.join(self.release_dir, archive)
+            target = os.path.join(self.pool_dir, archive)
+            if not os.path.exists(target):
+                shutil.move(source, target)
+                self.blather('Moved %s to %s' % (source, target))
+            if os.path.exists(source):
+                os.remove(source)
+            os.symlink(target, source)
+
         return all, pending
 
+    def __call__(self): #pragma NO COVERAGE
+        try:
+            all, pending = self.moveToPool()
+            self.blather(
+                "Updated %i out of %i archives" % (len(pending), len(all)))
+        except ValueError, e:
+            self.blather(str(e))
 
-def main():
+def _print(text): #pragma NO COVERAGE
+    print text
+
+def main(): #pragma NO COVERAGE
     try:
         update_pool = Pooler(sys.argv[1:])
     except ValueError, e:
@@ -108,5 +119,5 @@ def main():
         sys.exit(1)
     update_pool()
 
-if __name__ == '__main__':
+if __name__ == '__main__': #pragma NO COVERAGE
     main()
---
+++ compoze/tests/test_pooler.py
@@ -0,0 +1,181 @@
+import unittest
+
+class Test_is_archive(unittest.TestCase):
+
+    def _callFUT(self, filename):
+        from compoze.pooler import is_archive
+        return is_archive(filename)
+
+    def test_empty(self):
+        self.failIf(self._callFUT(''))
+
+    def test_bogus(self):
+        self.failIf(self._callFUT('nonesuch.txt'))
+
+    def test_tgz(self):
+        self.failIf(self._callFUT('foo_tgz'))
+        self.failUnless(self._callFUT('foo.tgz'))
+
+    def test_tar_gz(self):
+        self.failIf(self._callFUT('foo_tar.gz'))
+        self.failUnless(self._callFUT('foo.tar.gz'))
+
+    def test_tbz(self):
+        self.failIf(self._callFUT('foo_tbz'))
+        self.failUnless(self._callFUT('foo.tbz'))
+
+    def test_tar_bz2(self):
+        self.failIf(self._callFUT('foo_tar.bz2'))
+        self.failUnless(self._callFUT('foo.tar.bz2'))
+
+    def test_zip(self):
+        self.failIf(self._callFUT('foo_zip'))
+        self.failUnless(self._callFUT('foo.zip'))
+
+class PoolerTests(unittest.TestCase):
+
+    _tmpdirs = None
+
+    def tearDown(self):
+        if self._tmpdirs is not None:
+            import shutil
+            for x in self._tmpdirs:
+                shutil.rmtree(x)
+
+    def _getTargetClass(self):
+        from compoze.pooler import Pooler
+        return Pooler
+
+    def _makeOne(self, *args, **kw):
+        from optparse import Values
+        logger = kw.pop('logger', None)
+        default = kw.copy()
+        default.setdefault('verbose', False)
+        values = Values(default)
+        values.path = '.'
+        if logger is not None:
+            return self._getTargetClass()(values, logger=logger, *args)
+        return self._getTargetClass()(values, *args)
+
+    def _makeTempDir(self):
+        import tempfile
+        if self._tmpdirs is None:
+            self._tmpdirs = []
+        result = tempfile.mkdtemp()
+        self._tmpdirs.append(result)
+        return result
+
+    def _makeFile(self, dir, name, text='TEXT'):
+        import os
+        filename = os.path.join(dir, name)
+        f = open(filename, 'w')
+        f.write(text)
+        f.flush()
+        f.close()
+        return filename
+
+    def _makeLink(self, source, target, name):
+        import os
+        s_file = os.path.join(source, name)
+        t_file = os.path.join(target, name)
+        os.symlink(s_file, t_file)
+
+    def test_ctor_no_pool_dir_raises(self):
+        self.assertRaises(ValueError, self._makeOne)
+
+    def test_blather_not_verbose(self):
+        def _dont_go_here(*args):
+            assert 0, args
+        fetcher = self._makeOne('--quiet', self._makeTempDir(),
+                                logger=_dont_go_here)
+        fetcher.blather('foo') # doesn't assert
+
+    def test_blather_verbose(self):
+        logged = []
+        fetcher = self._makeOne('--verbose', self._makeTempDir(),
+                                logger=logged.append)
+        fetcher.blather('foo')
+        self.assertEqual(logged, ['foo'])
+
+    def test_listArchves_empty_releasedir(self):
+        release_dir = self._makeTempDir()
+        pool_dir = self._makeTempDir()
+        fetcher = self._makeOne('--quiet',
+                                '--path=%s' % release_dir,
+                                pool_dir,
+                               )
+        self.assertEqual(fetcher.listArchives(), ([], []))
+
+    def test_listArchves_non_empty_releasedir(self):
+        release_dir = self._makeTempDir()
+        self._makeFile(release_dir, 'foo.tar.gz')
+        pool_dir = self._makeTempDir()
+        fetcher = self._makeOne('--quiet',
+                                '--path=%s' % release_dir,
+                                pool_dir,
+                               )
+        self.assertEqual(fetcher.listArchives(),
+                        (['foo.tar.gz'], ['foo.tar.gz']))
+
+    def test_listArchves_already_in_pool(self):
+        release_dir = self._makeTempDir()
+        pool_dir = self._makeTempDir()
+        self._makeFile(pool_dir, 'foo.tar.gz')
+        self._makeLink(pool_dir, release_dir, 'foo.tar.gz')
+        fetcher = self._makeOne('--quiet',
+                                '--path=%s' % release_dir,
+                                pool_dir,
+                               )
+        self.assertEqual(fetcher.listArchives(),
+                        (['foo.tar.gz'], []))
+
+    def test_moveToPool_no_archives_in_release_dir_raises(self):
+        release_dir = self._makeTempDir()
+        pool_dir = self._makeTempDir()
+        fetcher = self._makeOne('--quiet',
+                                '--path=%s' % release_dir,
+                                pool_dir,
+                               )
+        self.assertRaises(ValueError, fetcher.moveToPool)
+
+    def test_moveToPool_invalid_pool_dir_raises(self):
+        release_dir = self._makeTempDir()
+        self._makeFile(release_dir, 'foo.tar.gz')
+        pool_dir = self._makeTempDir()
+        fetcher = self._makeOne('--quiet',
+                                '--path=%s' % release_dir,
+                                self._makeFile(pool_dir, 'foolish'),
+                               )
+        self.assertRaises(ValueError, fetcher.moveToPool)
+
+    def test_moveToPool_creates_pool_dir(self):
+        import os
+        release_dir = self._makeTempDir()
+        source = self._makeFile(release_dir, 'foo.tar.gz')
+        pool_dir = self._makeTempDir()
+        newdir = os.path.join(pool_dir, 'newdir')
+        fetcher = self._makeOne('--quiet',
+                                '--path=%s' % release_dir,
+                                newdir,
+                               )
+        fetcher.moveToPool()
+
+        self.failUnless(os.path.isdir(newdir))
+        self.failUnless(os.path.isfile(os.path.join(newdir, 'foo.tar.gz')))
+        self.failUnless(os.path.islink(source))
+
+    def test_moveToPool_existing_in_pool_dir(self):
+        import os
+        release_dir = self._makeTempDir()
+        source = self._makeFile(release_dir, 'foo.tar.gz', 'SOURCE')
+        pool_dir = self._makeTempDir()
+        target = self._makeFile(pool_dir, 'foo.tar.gz', 'TARGET')
+        fetcher = self._makeOne('--quiet',
+                                '--path=%s' % release_dir,
+                                pool_dir,
+                               )
+        fetcher.moveToPool()
+
+        self.failUnless(os.path.isfile(target))
+        self.assertEqual(open(target).read(), 'TARGET') # not replaced
+        self.failUnless(os.path.islink(source))
