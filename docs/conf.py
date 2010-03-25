14336fea76404da7340ef8065a5bc2e9fd19c20d
Tres Seaver tseaver@palladion.com <Tres Seaver tseaver@palladion.com@8f1d8bf8-68d2-4fbe-a113-2afb08c80ed9>
2010-03-25 11:00:37 +0000

    Bump documentation version.
    git-svn-id: svn+ssh://svn.repoze.org/svn/compoze/trunk@8605 8f1d8bf8-68d2-4fbe-a113-2afb08c80ed9

--- docs/conf.py
+++ docs/conf.py
@@ -45,9 +45,9 @@ copyright = '2009, Repoze Developers <repoze-dev@lists.repoze.org>'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '0.3'
 # The full version, including alpha/beta/rc tags.
-release = '0.1'
+release = '0.3'
 
 # There are two options for replacing |today|: either, you set today to
 # some non-false value, then it is used:
